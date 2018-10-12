773c8a3630dcdf6983881691bb35106ded383136
gevin1 <39243443+gevin1@users.noreply.github.com>
2018-10-12 22:53:22 +0800

    Update main2.js

--- demo/main2.js
+++ demo/main2.js
@@ -1,10 +1,5 @@
-var Http = "https://gevin1.github.io/gevin/demo/";
-var Path_func = ["main2.js","json.js","xiaoyi.js","run.js"];
-
 
 run = function (){
-	
-	Is_UpdataJs();
 	
 	include(scriptDir+"/xiaoyi.js");
 	
@@ -15,18 +10,3 @@ run = function (){
 	Main_run();
 	
 }();
-
-
-//检查是否需要更新
-function Is_UpdataJs(){
-	if(1 == Number(HttpUtil.get(Http+"updata.txt"))){
-		toast("检测到需要更新,请等待...");
-		for(a in Path_func){
-			print(FileUtil.write(scriptDir+"/"+Path_func[a],HttpUtil.get(Http+Path_func[a])));
-		};
-		sleep(2000);
-		toast("更新完成");
-	};
-}
-
-
